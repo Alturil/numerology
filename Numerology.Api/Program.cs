f11767e484ca9416aab1f76f9935fe7a1e8e5037
Alturil <alturil@gmail.com>
2024-05-16 14:53:36 +1200

    Add lambda deployment (#5)

--- Numerology.Api/Program.cs
+++ Numerology.Api/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 
 // Register services
 builder.Services.AddSingleton<ILettersNumberRepository, LettersNumberRepository>();
